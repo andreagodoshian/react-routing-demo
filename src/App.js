b8a1272b4e0d63c3115646c95bf402e58c1f0db7
Andrea <kvinna138@gmail.com>
2023-02-06 17:08:05 -0500

    Starting to add routing and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+const myRouter = createBrowserRouter([ // array of js objects
+  { path: "/", element: <HomePage/> },
+  { path: "/products", element: <ProductsPage/> }
+])
+
 function App() {
-  return <div></div>;
+  return (
+  <>
+    <RouterProvider router={myRouter}/>
+  </>
+  )
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+function HomePage() {
+    return(
+        <h1>Hej from the Home Page!</h1>
+    )
+}
+export default HomePage;
